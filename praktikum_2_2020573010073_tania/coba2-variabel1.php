dd191ece775890378a6bd75766867281ccbede01
taniaazzahra <tania.azwar13@gmail.com>
2021-09-14 08:19:56 +0700

    Menambahkan coba2-variabel1

---
+++ praktikum_2_2020573010073_tania/coba2-variabel1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible">
+        <meta name="viewport" content="width=device-widht,initial-scale-1.0">
+        <title>Document</title>
+    </head>
+<body>
+    <?php
+    $nama= "Salwa Nur";
+    echo "$nama berkata \"Katakan Namamu!\"";
+    ?>
+</body>
+</html>
