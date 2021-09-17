ed295db3a9e80e8405dac995cd61fd953c41a6a6
taniaazzahra <tania.azwar13@gmail.com>
2021-09-17 21:51:27 +0700

    Menambahkan coba4-operatorpenugasan

--- praktikum_2_2020573010073_tania/coba4-operatorpenugasan.php
+++ praktikum_2_2020573010073_tania/coba4-operatorpenugasan.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
-<?php
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
     $a = 3;
     $b = 7;
     $a += 5;
-    $b = ($c = 11) + 3;
+    $b = ($c = 11) +3;
     echo "Nilai variabel a adalah = $a";
     echo "<BR>";
     echo "Nilai variabel b adalah = $b";
     echo "<BR>";
     echo "Nilai variabel c adalah = $c";
-?>
+    ?>
+</body>
+</html>
