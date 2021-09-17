660d8e04cbb480544a86d5df1eb5ec01b8f6c7b1
taniaazzahra <tania.azwar13@gmail.com>
2021-09-17 21:35:14 +0700

    Menambahkan coba5-operasiaritmatika1

---
+++ praktikum_2_2020573010073_tania/coba5-operasiarimatika1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a=10;
+    $b=3;
+    echo "\$a = $a <BR>";
+    echo "\$b = $b <BR>";
+    echo '$a + $b = ';
+    print $a+$b;
+    echo "<BR>";
+    echo '$a - $b = ';
+    print $a-$b;
+    echo "<BR>";
+    echo '$a * $b = ';
+    print $a*$b;
+    echo "<BR>";
+    echo '$a / $b = ';
+    print $a/$b;
+    echo "<BR>";
+    echo '$a % $b = ';
+    print $a%$b;
+?>
+</body>
+</html>
