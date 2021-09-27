c5cc1a55deeaae10ed0ed8e8864030429a19c61e
taniaazzahra <tania.azwar13@gmail.com>
2021-09-27 10:16:43 +0700

    Menambahkan file coba5

---
+++ praktikum_4_2020573010073_tania/coba5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-widht, intial-scale=1.0">
+        <title>Document</title>
+    </head>
+<body>
+    <?php
+        $a = pow(2,10);  //Fungsi perpangkatan
+        $b = sqrt(100);  //Fungsi akar
+        $c = ceil(4,25);  //Pembulatan keatas
+        $d = floor(4,25);  //Pembulatan kebawah
+        echo "2 pangkat 10 = $a <br>";
+        echo "akar 100 = $b <br>";
+        echo "ceil (4.25) = $c <br>";
+        echo "floor = $d <br>";
+    ?>
+</body>
+</html>
