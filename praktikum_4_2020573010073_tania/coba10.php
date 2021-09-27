da6a7ac24c28b62920aa456d84b06c1c360cf325
taniaazzahra <tania.azwar13@gmail.com>
2021-09-27 10:57:07 +0700

    Menambahkan file coba10

---
+++ praktikum_4_2020573010073_tania/coba10.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-widht, intial-scale=1.0">
+        <title>Document</title>
+    </head>
+<body>
+    <?php
+        function  garis() {
+            echo "<hr>";
+        }
+        echo "Ini contoh fungsi yang tanpa parameter <br>";
+    garis();
+        echo "Lihat perbedaan dengan fungsi dengan parameter
+    <br"; garis();
+    ?>
+</body>
+</html>
