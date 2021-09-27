133cbc8c42c3afab5b8769012158157ce468469b
taniaazzahra <tania.azwar13@gmail.com>
2021-09-27 09:32:16 +0700

    Menambahkan file coba2

---
+++ praktikum_4_2020573010073_tania/coba2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-widht, intial-scale=1.0">
+        <title>Document</title>
+    </head>
+<body>
+    <?php
+        $nama[] = "Muhammad";
+        $nama[] = "Faruq"; $nama[] = "Zakiuddin"; echo
+    "Hallo";
+        echo "Isi array \$anak [0] adalah $nama[0]"; echo
+    "<br>";
+        echo "Isi array \$anak [1] adalah $nama[1]"; echo
+    "<br>";
+        echo "Isi array \$anak [2] adalah $nama[2]"; echo
+    "<br>";
+    ?>
+</body>
+</html>
