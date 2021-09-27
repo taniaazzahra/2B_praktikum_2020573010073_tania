f9308aa32ec020fc098c4b99946f52930aa025d1
taniaazzahra <tania.azwar13@gmail.com>
2021-09-27 10:41:23 +0700

    Menambahkan file coba8

---
+++ praktikum_4_2020573010073_tania/coba8.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-widht, intial-scale=1.0">
+        <title>Document</title>
+    </head>
+<body>
+    <?php
+        function psgpjg ($pjg, $lbr) {
+            $luas = $pjg * $lbr;
+            echo "$luas <br>";
+        }
+        $bil1 = 5; $bil2 = 3;
+        echo "Luas persegi panjang dengan pjg 5 dan lebar 3 
+        ="; psgpjg($bil1,$bil2);
+    ?>
+</body>
+</html>
