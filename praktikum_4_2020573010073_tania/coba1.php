6fdd0e9e4259984d7074e6738637bc3b4a8d8fea
taniaazzahra <tania.azwar13@gmail.com>
2021-09-27 09:19:33 +0700

    Menambahkan file coba1

---
+++ praktikum_4_2020573010073_tania/coba1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-widht, intial-scale=1.0">
+        <title>Document</title>
+    </head>
+<body>
+    <?php
+        $anak[0] = "Faruq";
+        $anak[1] = "Alya"; $anak[2] = "Zahro";
+        echo "Isi array \$anak[0] adalah $anak[0]"; echo
+    "<br>";
+        echo "Isi array \$anak[1] adalah $anak[1]"; echo
+    "<br>";
+        echo "Isi array \$anak[2] adalah $anak[2]"; echo
+    "<br>";
+    ?>
+</body>
+</html>
