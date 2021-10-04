e2f123200baa67659073b10f0d268ba51e65be34
taniaazzahra <tania.azwar13@gmail.com>
2021-10-04 09:29:56 +0700

    Menambahkan file press_login

---
+++ praktikum_5_2020573010073_tania/press_login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proses Input</title>
+</head>
+<body>
+    <?php 
+        $username=$_POST["username"];
+        $password=$_POST["password"];
+    ?>
+    Username : <?echo $username?>
+    <br>
+    Password : </echo $password?>
+    <br>
+</body>
+</html>
