6f24234d3be4964940fb891224ee43389dc1f8e8
taniaazzahra <tania.azwar13@gmail.com>
2021-10-04 09:35:05 +0700

    Menambahkan file get_post

---
+++ praktikum_5_2020573010073_tania/met_post.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form method POST</title>
+</head>
+<body>
+    <h1>Input</h1>
+    <form action = "met_postproses.php" method="post">
+        Alamat : <input type "text" name="alamat" size="50">
+        <input type="submit" value="Simpan">
+    </form>
+</body>
+</html>
